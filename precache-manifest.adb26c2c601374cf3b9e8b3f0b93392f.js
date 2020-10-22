674bc2d9012ec3509c93bb3beea81dae9e09be45
stevenclarke94 <stevenjsclarke@yahoo.co.uk>
2020-10-22 20:12:47 +0100

    Updates

---
+++ precache-manifest.adb26c2c601374cf3b9e8b3f0b93392f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f54de091d020a49564d95ed695bbf699",
+    "url": "/todo3/index.html"
+  },
+  {
+    "revision": "90989faca70ed09b481c",
+    "url": "/todo3/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "b27cd31348508e90c4d1",
+    "url": "/todo3/static/css/main.9e156813.chunk.css"
+  },
+  {
+    "revision": "90989faca70ed09b481c",
+    "url": "/todo3/static/js/2.b61dbe77.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/todo3/static/js/2.b61dbe77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b27cd31348508e90c4d1",
+    "url": "/todo3/static/js/main.7f04dd1e.chunk.js"
+  },
+  {
+    "revision": "9bb7ea325b7681b34d8b",
+    "url": "/todo3/static/js/runtime-main.c182643a.js"
+  },
+  {
+    "revision": "28da59c8861d2648fe51842dbb6a477f",
+    "url": "/todo3/static/media/computer.28da59c8.png"
+  },
+  {
+    "revision": "54a1b4f35cdb2aa866b463e0b31bcb6f",
+    "url": "/todo3/static/media/todobackgroundyellow.54a1b4f3.jpg"
+  }
+]);
